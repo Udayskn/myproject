059ae72853e9c728aedaea8e5b89496d51c63d42
Udayskn <kishoreisat16@gmail.com>
2025-03-26 17:53:05 +0530

    backend complete

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=100)
-    rollnumber = models.CharField(max_length=10,unique=True)
+    rollnumber = models.CharField(max_length=50,unique=True)
     cgpa = models.FloatField()
     def __str__(self):
         return f"{self.name} ({self.rollnumber})"
--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .serializers import StudentSerializer
+from rest_framework import viewsets,filters
+from .models import Student
 # Create your views here.
+
+class StudentViewSet(viewsets.ModelViewSet) :
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    
+    filter_backends = [filters.SearchFilter,filters.OrderingFilter]
+    search_fields = ['name','rollnumber']
+    ordering_fields = ['rollnumber','cgpa']
+
